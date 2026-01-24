d4290069e20fc1cf9f98ca7f1f53a44ea47be9e6
Darnley Sanon <9lmntstudio@gmail.com>
2026-01-24 00:05:42 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./app"; // 1. Capitalized 'App' to match the component below
+import "./index.css";
 
-  import { createRoot } from "react-dom/client";
-  import app from "./app.tsx";
-  import "./index.css";
-
-  createRoot(document.getElementById("root")!).render(<app />);
-  
+// 2. Used the 'createRoot' variable directly as imported above
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
