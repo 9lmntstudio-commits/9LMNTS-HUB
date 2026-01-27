17e3b266246a0fdfeeab69618d59a09d51e9cb15
Darnley Sanon <9lmntstudio@gmail.com>
2026-01-26 21:40:14 -0500

    Change import to use @jsr/supabase__supabase-js

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@jsr/supabase__supabase-js';
 
 const supabaseUrl = 'https://cnlwahugppuvakjqkvjy.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNubHdhaHVncHB1dmFranFrdmp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg2NzUyNjksImV4cCI6MjA4NDI1MTI2OX0.dalAGA1QkeovGTq_umcf45YypTuJL5sWlwb6IlXb9zc';
