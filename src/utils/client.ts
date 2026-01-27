b5976d830e0344f01041fb53ea611cc18e656284
Darnley Sanon <9lmntstudio@gmail.com>
2026-01-26 21:15:21 -0500

    Change Supabase URL and Anon Key
    Updated Supabase URL and Anon Key in client.ts.

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://vfrxxfviaykafzbxpehw.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'YOUR_ANON_KEY'; // Replace YOUR_ANON_KEY with your actual Supabase Anon Key
+const supabaseUrl = 'https://cnlwahugppuvakjqkvjy.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNubHdhaHVncHB1dmFranFrdmp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg2NzUyNjksImV4cCI6MjA4NDI1MTI2OX0.dalAGA1QkeovGTq_umcf45YypTuJL5sWlwb6IlXb9zc';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
